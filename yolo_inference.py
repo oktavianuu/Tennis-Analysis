0b72f5b451485a34f961acd9d9b7ca437035bf88
Oktavianu Misro Adrianto <oktavianu@192.168.1.80>
2024-10-25 11:02:53 +0700

    track

--- yolo_inference.py
+++ yolo_inference.py
@@ -2,9 +2,9 @@ from ultralytics import YOLO
 
 model = YOLO('yolov8x')
 
-result = model.predict('data_sample/sample_video.mp4', save=True)
-print(result)
-print("boxes:")
-for box in result[0].boxes:
-    print(box)
+result = model.track('data_sample/sample_video.mp4', conf=0.2, save=True)
+# print(result)
+# print("boxes:")
+# for box in result[0].boxes:
+#     print(box)
 
