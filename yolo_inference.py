89c9c63e6b25805f1189ef641865130cd3d92e2e
oktavianu <oktavianuss.misro@gmail.com>
2024-10-25 21:36:02 +0700

    add models predicted video file

--- yolo_inference.py
+++ yolo_inference.py
@@ -1,8 +1,8 @@
 from ultralytics import YOLO
 
-model = YOLO('yolov8x')
+model = YOLO('models/last.pt')
 
-result = model.track('data_sample/sample_video.mp4', conf=0.2, save=True)
+result = model.predict('data_sample/sample_video.mp4', conf=0.2, save=True)
 # print(result)
 # print("boxes:")
 # for box in result[0].boxes:
